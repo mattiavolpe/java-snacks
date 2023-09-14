774d4901fec7aed0061c49f2c49c76f225a0523c
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-09-14 12:22:35 +0200

    Snack 5

---
+++ src/org/java/snacks/Snack5.java
@@ -0,0 +1,32 @@
+package org.java.snacks;
+
+import java.util.Scanner;
+
+public class Snack5 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Insert a sentence or a word: ");
+		String sentence = sc.nextLine();
+		
+		int alphabeticalCounter = 0;
+		int numericCounter = 0;
+		int symbolsCounter = 0;
+		
+		for (int i = 0; i < sentence.length(); i++) {
+			char currentChar = sentence.charAt(i);
+			
+			String singleCharacter = Character.toString(currentChar);
+
+			if (singleCharacter.matches("[a-zA-Z]")) {
+				alphabeticalCounter++;
+			} else if (singleCharacter.matches("[0-9]")) {
+				numericCounter++;
+			} else if (singleCharacter.matches("\\S")) {
+				symbolsCounter++;
+			}
+		}
+		
+		System.out.println("The word / sentence " + sentence + " has: " + alphabeticalCounter + " alphabetical characters, " + numericCounter + " numbers and " + symbolsCounter + " symbols");
+	}
+}

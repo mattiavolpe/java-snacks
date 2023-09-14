6fbcf2f465f90253acff0a43ecda603890017c4a
mattiavolpe <64392380+mattiavolpe@users.noreply.github.com>
2023-09-14 12:40:24 +0200

    ADD exiting at 0 on Snack 5

--- src/org/java/snacks/Snack5.java
+++ src/org/java/snacks/Snack5.java
@@ -5,28 +5,33 @@ import java.util.Scanner;
 public class Snack5 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		String sentence = null;
 		
-		System.out.print("Insert a sentence or a word: ");
-		String sentence = sc.nextLine();
-		
-		int alphabeticalCounter = 0;
-		int numericCounter = 0;
-		int symbolsCounter = 0;
-		
-		for (int i = 0; i < sentence.length(); i++) {
-			char currentChar = sentence.charAt(i);
+		do {
+			System.out.print("Insert a sentence or a word: ");
+			sentence = sc.nextLine();
 			
-			String singleCharacter = Character.toString(currentChar);
+			if (sentence.trim().equals("0")) return;
+			
+			int alphabeticalCounter = 0;
+			int numericCounter = 0;
+			int symbolsCounter = 0;
+			
+			for (int i = 0; i < sentence.length(); i++) {
+				char currentChar = sentence.charAt(i);
+				
+				String singleCharacter = Character.toString(currentChar);
 
-			if (singleCharacter.matches("[a-zA-Z]")) {
-				alphabeticalCounter++;
-			} else if (singleCharacter.matches("[0-9]")) {
-				numericCounter++;
-			} else if (singleCharacter.matches("\\S")) {
-				symbolsCounter++;
+				if (singleCharacter.matches("[a-zA-Z]")) {
+					alphabeticalCounter++;
+				} else if (singleCharacter.matches("[0-9]")) {
+					numericCounter++;
+				} else if (singleCharacter.matches("\\S")) {
+					symbolsCounter++;
+				}
 			}
-		}
-		
-		System.out.println("The word / sentence " + sentence + " has: " + alphabeticalCounter + " alphabetical characters, " + numericCounter + " numbers and " + symbolsCounter + " symbols");
+			
+			System.out.println("The word / sentence " + sentence + " has: " + alphabeticalCounter + " alphabetical characters, " + numericCounter + " numbers and " + symbolsCounter + " symbols");
+		} while (!sentence.trim().equals("0")); // while (true) ALSO WORKS BECAUSE OF THE return AT ROW 14
 	}
 }
---
+++ src/org/java/snacks/Snack6.java
@@ -0,0 +1,17 @@
+package org.java.snacks;
+
+import java.util.Scanner;
+
+public class Snack6 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Type a number: ");
+		String numericString = sc.nextLine();
+		
+		int number = 0;
+		for (int i = 0; i < numericString.length(); i++) {
+			
+		}
+	}
+}
